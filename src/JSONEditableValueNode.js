a1fa99e9ed47789dc175a686123f8c4631b9aac8
hsbacot <hsbacot@gmail.com>
2016-05-23 09:17:23 -0400

    made smart to manage key updates and making the component updatable locally for more responsive performance

--- src/JSONEditableValueNode.js
+++ src/JSONEditableValueNode.js
@@ -1,32 +1,92 @@
 import React, { PropTypes } from 'react';
 
-/**
- * Renders simple values (eg. strings, numbers, booleans, etc)
- */
 
-const JSONEditableValueNode = ({
-  nodeType,
-  styling,
-  labelRenderer,
-  keyPath,
-  valueRenderer,
-  value,
-  valueGetter,
-  handleClick,
-  updateValue
-  }) => (
-  <li
-    {...styling('value', nodeType, keyPath)}
-  >
-    <label {...styling(['label', 'valueLabel'], nodeType, keyPath)}>
-      {labelRenderer(...keyPath)}:
-    </label>
-    <input {...styling('valueText', nodeType, keyPath)}
-      onClick={handleClick}
-      onChange={(e) => updateValue(e.target.value, keyPath)}
-      value={valueRenderer(valueGetter(value), value)}/>
-  </li>
-);
+export class JSONEditableValueNode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editableKey: false,
+      editableValue: false,
+      keyValue: this.props.labelRenderer(...this.props.keyPath)
+    }
+
+  }
+
+  makeEditableKey = () => {
+    console.log('kasdf');
+    this.setState({
+      editableKey: true
+    })
+  };
+
+  makeEditableValue = () => {
+    this.setState({
+      editableValue: true
+    })
+  };
+
+  clickUpdateValue = () => {
+    this.setState({
+      editableKey: false,
+      editableValue: false
+    });
+    this.props.updateNodeKey(this.state.keyValue, this.props.keyPath);
+  };
+
+  updateLocalKey = (newKey) => {
+    this.setState({
+      keyValue: newKey
+    })
+  };
+
+  render() {
+    const {
+      nodeType,
+      styling,
+      labelRenderer,
+      keyPath,
+      valueRenderer,
+      value,
+      valueGetter,
+      updateValue,
+      updateNodeKey,
+      updateNodeValue,
+      removeNode,
+      } = this.props;
+
+      const editableKey = this.state.editableKey ?
+        <input {...styling(['label', 'valueLabel'], nodeType, keyPath)}
+          value={this.state.keyValue}
+          onChange={(e) => this.updateLocalKey(e.target.value, keyPath)} /> :
+        <label {...styling(['label', 'valueLabel'], nodeType, keyPath)} onClick={this.makeEditableKey}>
+          {labelRenderer(...keyPath)}
+        </label>;
+
+       const editableValue = this.state.editableValue ?
+        <input {...styling('valueText', nodeType, keyPath)}
+            onChange={(e) => updateValue(e.target.value, keyPath)}
+            value={valueRenderer(valueGetter(value), value)}/> :
+          <span {...this.props.styling('valueText', nodeType, keyPath)} onClick={this.makeEditableValue}>
+            {valueRenderer(valueGetter(value), value)}
+          </span>;
+
+      const actionButton = this.state.editableKey || this.state.editableValue ?
+        <button onClick={this.clickUpdateValue}>Update Value</button> :
+        <button onClick={() => removeNode(keyPath)}>X</button>;
+
+    return (
+      <div>
+        <li
+          {...this.props.styling('value', nodeType, keyPath)}
+        >
+          { editableKey }:
+          { editableValue }
+          { actionButton }
+        </li>
+      </div>
+    )
+  }
+}
 
 JSONEditableValueNode.propTypes = {
   nodeType: PropTypes.string.isRequired,
@@ -37,12 +97,15 @@ JSONEditableValueNode.propTypes = {
   ).isRequired,
   valueRenderer: PropTypes.func.isRequired,
   value: PropTypes.any,
-  valueGetter: PropTypes.func
+  valueGetter: PropTypes.func,
+  makeEditable: PropTypes.func,
+  editableNode: PropTypes.arrayOf(
+    PropTypes.oneOfType([PropTypes.string, PropTypes.number]))
 };
 
 JSONEditableValueNode.defaultProps = {
   valueGetter: value => value,
-  handleClick: () => console.log('hunter')
+  editableNode: ''
 };
 
 export default JSONEditableValueNode;
